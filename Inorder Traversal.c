be1e81df3920e7e75bceb74e0d283ebf15f87bbc
seongjieun <107239242+seongjieun@users.noreply.github.com>
2023-05-08 16:02:42 +0900

    Update Inorder Traversal.c

--- Inorder Traversal.c
+++ Inorder Traversal.c
@@ -1,9 +1,9 @@
 void Inorder(BTreeNode* root)
 {
-if (root != NULL;
-{
-Inorder(root->left_child);
-printf("%d", root->item);
-Inorder(root->right_child);
-}
+    if (root != NULL);
+    {
+        Inorder(root->left_child);
+        printf("%d", root->item);
+        Inorder(root->right_child);
+    }
 }
