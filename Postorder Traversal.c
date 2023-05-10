7d8850b47bf5fa06e774b72229f46b1b5a0a73bf
seongjieun <107239242+seongjieun@users.noreply.github.com>
2023-05-10 10:56:19 +0900

    Create Postorder Traversal.c

---
+++ Postorder Traversal.c
@@ -0,0 +1,11 @@
+#Postorder Traversal (LRC)
+
+void Postorder(BTreeNode *root)
+{
+  if(root!= NULL)
+  {
+  Postorder(root->left_child);
+  Postorder(root->right_child),
+  printf("%d", root->item);
+  }
+}
