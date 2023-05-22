a0cc2f46f7a0ed6b0d28481b634e9a371e76b383
seongjieun <107239242+seongjieun@users.noreply.github.com>
2023-05-22 16:10:33 +0900

    Update Implementation of Insertion.c
    주석추가

--- priority Queue and Heap/Implementation of Insertion.c
+++ priority Queue and Heap/Implementation of Insertion.c
@@ -1,10 +1,10 @@
 void Insert(Heap *pheap, Data data, int priority){
   HNode newNode;
-  int idx=pheap->num+1;//
+  int idx=pheap->num+1;//새로 넣으려는 위치(맨끝)
   if(IsFull(pheap)) exit(1);
   //Compare the new node with its parent.
   while(idx>1){
-    int parent=GetParent(idx);
+    int parent=GetParent(idx); //부모 인덱스 찾는 법: 현재 node 나누기 2
     if(priority>pheap->items[parent].priority){
       pheap->items[idx]=pheap->items[parent];
       idx=parent;
