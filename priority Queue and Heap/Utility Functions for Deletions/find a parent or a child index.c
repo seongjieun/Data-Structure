58d0b56e604113c0b5509aa91d2d23f4a3c0b609
seongjieun <107239242+seongjieun@users.noreply.github.com>
2023-06-20 00:16:30 +0900

    Update find a parent or a child index.c

--- priority Queue and Heap/Utility Functions for Deletions/find a parent or a child index.c
+++ priority Queue and Heap/Utility Functions for Deletions/find a parent or a child index.c
@@ -1,14 +1,14 @@
 //Get a parent index for a given index.
 int GetParent(int idx){
-  return idx/2;
+  return idx / 2;
 }
 
 //Get a left child index for a given index.
 int GetLChild(int idx){
-  return idx*2;
+  return idx * 2;
 }
 
 //Get a right child index for a given idex.
 int GetRChild(int idx){
-  return idx*2+1;
+  return idx * 2 + 1;
 }
