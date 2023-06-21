4fb6aaadbd6a927d8ed8aa5f80cf4a0d8194cfd7
seongjieun <107239242+seongjieun@users.noreply.github.com>
2023-06-21 10:55:39 +0900

    Update Initializing Heap.c

--- priority Queue and Heap/Initializing Heap.c
+++ priority Queue and Heap/Initializing Heap.c
@@ -1,14 +1,14 @@
-//Make a heap empty.
+// Make a heap empty.
 void InitHeap(Heap *pheap){
   pheap->num=0;
 }
 
-//Check whether a heap is empty.
+// Check whether a heap is empty.
 bool IsEmpty(Heap *pheap){
   return pheap->num==0;
 }
 
-//Check whether a heap is full.
+// Check whether a heap is full.
 bool IsFull(Heap *pheap){
   return pheap->num==MAX_HEAP;
 }
